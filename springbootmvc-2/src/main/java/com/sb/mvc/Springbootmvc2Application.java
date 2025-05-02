8ef713e9b0c8099a45f732023dfda5ea440852cd
Hritika Patil <hritikapatil14@gmail.com>
2025-05-02 11:25:25 +0530

    first commit

---
+++ springbootmvc-2/src/main/java/com/sb/mvc/Springbootmvc2Application.java
@@ -0,0 +1,13 @@
+package com.sb.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootmvc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootmvc2Application.class, args);
+	}
+
+}
---
+++ springbootmvc-2/src/test/java/com/sb/mvc/Springbootmvc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootmvc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
